6dda40f1c0f8082da2c0e1f3d7070ee2fe470008
InesLalou <58553243+InesLalou@users.noreply.github.com>
2022-12-26 16:35:57 +0100

    Update RedisApplication.java

--- src/main/java/com/ECE/redis/RedisApplication.java
+++ src/main/java/com/ECE/redis/RedisApplication.java
@@ -43,7 +43,7 @@ public class RedisApplication {
         SpringApplication.run(RedisApplication.class, args);
 	    
 	RedisJava cache = new RedisJava();
-        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
+        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(42));
 
         System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
     }
