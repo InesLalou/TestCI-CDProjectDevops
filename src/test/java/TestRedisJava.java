8272747df98710c69ffd5e6e311dbf3c1e72141c
InesLalou <58553243+InesLalou@users.noreply.github.com>
2022-12-27 00:26:43 +0100

    Update TestRedisJava.java

--- src/test/java/TestRedisJava.java
+++ src/test/java/TestRedisJava.java
@@ -14,7 +14,7 @@ public class TestRedisJava {
         //DeepThought ourSuperComputer = new DeepThought();
         //int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
 
-        RedisJava cache = new RedisJava();
+        RedisJavaz cache = new RedisJavaz();
         cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(42));
 
         //assertEquals("42", cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
