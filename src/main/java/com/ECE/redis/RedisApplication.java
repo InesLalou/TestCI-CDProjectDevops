1611f31482787cb16d72e5165a04174c003fc532
InesLalou <58553243+InesLalou@users.noreply.github.com>
2022-12-26 02:18:39 +0100

    Update RedisApplication.java

--- src/main/java/com/ECE/redis/RedisApplication.java
+++ src/main/java/com/ECE/redis/RedisApplication.java
@@ -2,6 +2,7 @@ package com.ECE.redis;
 
 import com.ECE.redis.entity.User;
 import com.ECE.redis.respository.UserAction;
+import com.ECE.redis.respository.RedisJava;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
