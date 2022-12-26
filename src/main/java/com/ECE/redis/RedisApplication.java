8958f21fb636cd469a509cacd88f0ddee4ca448c
InesLalou <58553243+InesLalou@users.noreply.github.com>
2022-12-26 02:09:29 +0100

    redis cjanges test main

--- src/main/java/com/ECE/redis/RedisApplication.java
+++ src/main/java/com/ECE/redis/RedisApplication.java
@@ -40,6 +40,11 @@ public class RedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
+	    
+	RedisJava cache = new RedisJava();
+        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
+
+        System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
     }
 
 }
